56da130eeaad692872b3c84b96e6c59d19a390fe
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-02-19 09:20:22 -0800

    first commit

---
+++ modules/people/people.service.js
@@ -0,0 +1,26 @@
+const Queue = require('../queue/Queue')
+const store = require('../../store')
+
+// Set up initial data.
+// --------------------
+
+const people = new Queue()
+store.people.forEach(person => people.enqueue(person))
+
+// --------------------
+
+module.exports = {
+  get() {
+    return people.all();
+  },
+
+  enqueue(person) {
+    // Add a person to the queue.
+    people.enqueue(person);
+  },
+
+  dequeue() {
+    // Remove a person from the queue.
+    people.dequeue();
+  }
+}
---
+++ modules/pets/pets.service.js
@@ -0,0 +1,35 @@
+const Queue = require('../queue/Queue')
+const store = require('../../store')
+const { cats, dogs } = require('../../store')
+const peopleService = require('../people/people.service')
+
+// Set up initial data.
+// --------------------
+
+const pets = {
+  cats: new Queue(),
+  dogs: new Queue()
+}
+
+store.cats.forEach(cat => pets.cats.enqueue(cat))
+store.dogs.forEach(dog => pets.dogs.enqueue(dog))
+
+// --------------------
+
+module.exports = {
+  get() {
+    // Return the pets next in line to be adopted.
+    let cat = pets.cats.show();
+    let dog = pets.dogs.show();
+    let available = [cat?{cat}:null,  dog?{dog}:null];
+    return available;
+  },
+
+  dequeue(type) {
+    // Remove a pet from the queue.
+    if (type === 'dog') {
+      pets.dogs.dequeue();
+    }
+  pets.cats.dequeue();
+  },
+};
